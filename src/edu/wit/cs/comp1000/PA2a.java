80c5e9095560e0c142f4fd88e07344ea15eb90aa
Jacob Lopez <lopezj25@wit.edu>
2024-10-16 13:12:37 -0400

    pa2 Assignment Jacob Lopez

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,189 @@
 package edu.wit.cs.comp1000;
 
+import java.util.Scanner;
 // TODO: document this class
 public class PA2a {
 
-	// TODO: document this function
-	public static void main(String[] args) {
-		// TODO: write your code here
-	}
+	
+	    public static void main(String[] args) {
+	    	Scanner input = new Scanner(System.in);
+	        
 
-}
+	            System.out.print("Enter five whole numbers: ");
+
+	            int inOne = input.nextInt();
+
+	            int inTwo = input.nextInt();
+
+	            int inThree = input.nextInt();
+
+	            int inFour = input.nextInt();
+
+	            int inFive = input.nextInt();
+
+	            int sum = 0;
+
+	            int sumN = 0;
+
+	            int sumCount = 0;
+
+	            int sumNCount = 0;
+
+	           
+
+	 
+
+	            if( inOne > 0 ){
+
+	                sum += inOne;
+
+	                sumCount++;
+
+	            }
+
+	                else{
+
+	                    sumN += inOne;
+
+	                    sumNCount++;
+
+	                }
+
+	            if( inTwo> 0 ){
+
+	                sum += inTwo;
+
+	                sumCount++;
+
+	            }
+
+	                else{
+
+	                    sumN += inTwo;
+
+	                    sumNCount++;
+
+	                }
+
+	            if( inThree > 0 ){
+
+	                sum += inThree;
+
+	                sumCount++;
+
+	            }
+
+	                else{
+
+	                    sumN += inThree;
+
+	                    sumNCount++;
+
+	                }
+
+	            if( inFour > 0 ){
+
+	                sum += inFour;
+
+	                sumCount++;
+
+	            }
+
+	                else{
+
+	                    sumN += inFour;
+
+	                    sumNCount++;
+
+	                }
+
+	            if( inFive > 0 ){
+
+	                sum += inFive;
+
+	                sumCount++;
+
+	            }
+
+	                else{
+
+	                    sumN += inFive;
+
+	                    sumNCount++;
+
+	                }
+
+	            int sumAll = sum + sumN;
+
+	            double sumDouble = sum;
+
+	            double sumNDouble = sumN;
+
+	            double sumAvg = (sumCount > 0) ? (double) sum/sumCount : 0.0;
+
+	            double sumNAvg = (sumNCount > 0) ? (double) sumN/sumNCount : 0.0;
+
+	            double sumAvgAll = (sumDouble + sumNDouble) / 5;
+
+	            if(sumCount != 1){
+
+	                System.out.println("The sum of the " + sumCount  + " positive numbers: " + sum);
+
+	            }
+
+	            else{
+
+	                System.out.println("The sum of the " + sumCount  + " positive number: " + sum);
+
+	            }
+
+	            if(sumNCount != 1){
+
+	                System.out.println("The sum of the " + sumNCount + " non-positive numbers: " + sumN);
+
+	            }
+
+	            else{
+
+	                System.out.println("The sum of the " + sumNCount + " non-positive number: " + sumN);
+
+	            }
+
+	            System.out.println("The sum of the 5 numbers: " + sumAll);
+
+	            if(sumCount != 1){
+
+	                System.out.print("The average of the " + sumCount);
+
+	                System.out.printf(" positive numbers: %.2f%n" , sumAvg);
+
+	            }
+
+	            else{
+
+	                System.out.print("The average of the " + sumCount);
+
+	                System.out.printf(" positive number: %.2f%n" , sumAvg);
+
+	            }
+
+	            if(sumNCount != 1){
+
+	                System.out.print("The average of the " + sumNCount);
+
+	                System.out.printf(" non-positive numbers: %.2f%n" , sumNAvg);
+
+	            }
+
+	            else{
+
+	                System.out.print("The average of the " + sumNCount);
+
+	                System.out.printf(" non-positive number: %.2f%n" , sumNAvg);
+
+	            }
+
+	            System.out.printf("The average of the 5 numbers: %.2f%n" , sumAvgAll);
+	            
+	        }
+	    }
--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -1,11 +1,50 @@
 package edu.wit.cs.comp1000;
 
+import java.util.Scanner;
 // TODO: document this class
 public class PA2b {
 
 	// TODO: document this function
-	public static void main(String[] args) {
-		// TODO: write your code here
-	}
+	 public static void main(String[] args) {
 
+	        Scanner input = new Scanner(System.in);
+	        
+
+	            System.out.printf("Enter a b c: ");
+
+	            float a = input.nextFloat();
+
+	            float b = input.nextFloat();
+
+	            float c = input.nextFloat();
+
+	            float d = (float) (Math.pow(b,2))-(4*a*c);
+
+	 
+
+	            if(d<0) {
+
+	                System.out.printf("Roots: imaginary%n");
+
+	            }
+
+	            else if(d == 0) {
+
+	                float root = -b/2*a;
+
+	                System.out.printf("Root: %.2f%n", root);
+
+	            }
+
+	            else{
+
+	                float roota = (float) (((-b)-Math.sqrt(d))/(2.0*a));
+
+	                float rootb = (float) (((-b)+Math.sqrt(d))/(2.0*a));
+
+	                System.out.printf("Roots: %.2f, %.2f%n", roota, rootb);
+
+	            }
+
+}
 }
